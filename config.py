e488dce9c91457998dacd04d3fa6618bcaff3108
Rock Chin <1010553892@qq.com>
2022-07-09 21:32:57 +0800

    pref: 完善config.py中的注释

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 import logging
 
 # QQ机器人账号
+# 务必与Mirai-console登录的QQ一致
 qq_bot_uin = 0
 
 # mirai-http的verifykey
@@ -11,18 +12,24 @@ mirai_http_verify_key = 'yirimirai'
 qzone_uin = 0
 
 # 预置Qzone cookie,若没有,将会在启动时请求管理员扫码
+# 如果预置的cookie不可用,将会自动请求管理员扫码
+# 扫码登录之后,cookie会被写入此字段
 qzone_cookie = ''
 
 # 自动回复消息
+# 用户私聊发送消息给机器人时,机器人会回复此消息及小程序码
 auto_reply_message = '[bot]bot收到私发QQ消息时回复的文字\n\n'
 
 # 小程序码路径
+# 用于自动回复消息,若此文件不存在,将仅回复文字消息
 qrcode_path = './qrcode.jpg'
 
 # 管理员QQ，用于接收系统内部通知
+# 支持多个管理员,用逗号分隔
 admin_uins = [1111111111]
 
 # 管理群，用于审核说说
+# 支持多个管理群,用逗号分隔
 admin_groups = [1234567890]
 
 # MySQL数据库
@@ -38,6 +45,8 @@ database_context = {
 api_port = 8989
 
 # RESTful API 域名
+# 此字段并不是必须的,如果不填写,将使用http协议进行监听
+# 填写之后,启动时将认证证书是否匹配
 api_domain = 'localhost'
 
 # RESTful API SSL 证书路径
@@ -46,6 +55,7 @@ api_ssl_context = (
     './cert/key.pem'
 )
 
+# 小程序相关字段的获取方式请查阅微信官方文档
 # 小程序云开发的环境id
 cloud_env_id = 'dev-0'
 
