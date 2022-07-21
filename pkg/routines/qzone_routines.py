fde69ae5cb9b2d4c3d8e370da7a8138d90f852c5
Rock Chin <1010553892@qq.com>
2022-07-21 23:37:31 +0800

    fix: 短时间内通过多个稿件时被重复发表

--- pkg/routines/qzone_routines.py
+++ pkg/routines/qzone_routines.py
@@ -18,26 +18,34 @@ def login_via_qrcode_callback(path):
     ])
 
 
+mutex = threading.Lock()
+
+
 # 发表所有正在等待的posts
 def clean_pending_posts(interval_seconds=10):
-    db_inst = pkg.database.database.get_inst()
-    posts_data = db_inst.pull_posts(status='通过')
-    for post in posts_data['posts']:
-        # print("正在发送",post)
-        try:
-            start_ts = time.time()
-            tid = pkg.qzone.publisher.get_inst().prepare_post(post)
-            # 发表完成
-            finish_ts = time.time()
-            # pkg.chat.manager.get_inst().send_message_to_admins(
-            #     "[bot]已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
-            logging.info("已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
-            pkg.database.database.get_inst().update_post_status(post_id=post['id'], new_status='已发表')
+    try:
+        mutex.acquire()
+        db_inst = pkg.database.database.get_inst()
+        posts_data = db_inst.pull_posts(status='通过')
+        for post in posts_data['posts']:
+            # print("正在发送",post)
+            try:
+                start_ts = time.time()
+                tid = pkg.qzone.publisher.get_inst().prepare_post(post)
+                # 发表完成
+                finish_ts = time.time()
+                # pkg.chat.manager.get_inst().send_message_to_admins(
+                #     "[bot]已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
+                logging.info("已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
+                pkg.database.database.get_inst().update_post_status(post_id=post['id'], new_status='已发表')
 
-            threading.Thread(target=pkg.routines.post_routines.post_finished, args=(post['id'], post['qq'], tid)).start()
-        except Exception as e:
-            pkg.chat.manager.get_inst().send_message_to_admins("##{}发表失败:{}".format(post['id'], e))
-            pkg.database.database.get_inst().update_post_status(post_id=post['id'], new_status='失败',
-                                                                review=str(e)[:120])
-            logging.exception(e)
-        time.sleep(interval_seconds)
+                threading.Thread(target=pkg.routines.post_routines.post_finished,
+                                 args=(post['id'], post['qq'], tid)).start()
+            except Exception as e:
+                pkg.chat.manager.get_inst().send_message_to_admins("##{}发表失败:{}".format(post['id'], e))
+                pkg.database.database.get_inst().update_post_status(post_id=post['id'], new_status='失败',
+                                                                    review=str(e)[:120])
+                logging.exception(e)
+            time.sleep(interval_seconds)
+    finally:
+        mutex.release()
