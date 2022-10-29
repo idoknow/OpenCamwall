ca4c6822df28c353eeafd2dab7db0d124e6055ce
Rock Chin <1010553892@qq.com>
2022-10-29 13:14:23 +0800

    perf: 发表完成后消息到管理员群

--- pkg/routines/qzone_routines.py
+++ pkg/routines/qzone_routines.py
@@ -41,15 +41,15 @@ def clean_pending_posts(interval_seconds=10):
                 tid = pkg.qzone.publisher.get_inst().prepare_and_publish_post(post)
                 # 发表完成
                 finish_ts = time.time()
-                # pkg.chat.manager.get_inst().send_message_to_admins(
-                #     "[bot]已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
+                pkg.chat.manager.get_inst().send_message_to_admin_groups(
+                    "[bot]已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
                 logging.info("已完成发表:##{} 耗时:{:.2f}s".format(post['id'], finish_ts - start_ts))
                 pkg.database.database.get_inst().update_post_status(post_id=post['id'], new_status='已发表')
 
                 threading.Thread(target=pkg.routines.post_routines.post_finished,
                                  args=(post['id'], post['qq'], tid)).start()
             except Exception as e:
-                pkg.chat.manager.get_inst().send_message_to_admins("##{}发表失败:{}".format(post['id'], e))
+                pkg.chat.manager.get_inst().send_message_to_admin_groups("##{}发表失败:{}".format(post['id'], e))
                 pkg.database.database.get_inst().update_post_status(post_id=post['id'], new_status='失败',
                                                                     review=str(e)[:120])
                 logging.exception(e)
